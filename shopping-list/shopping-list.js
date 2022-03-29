70204511db08ac7ee4fe6b06f352e5aa0abb7923
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-29 12:50:56 -0700

    wrote form event listener

--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -1,12 +1,31 @@
 import { 
     checkAuth, 
     logout,
-    // createItem,
+    createItem,
     // deleteAllItems,
     // deleteItem,
     // updateItem,
     // getItems
 } from '../fetch-utils.js';
+import {
+    // renderItem,
+    // renderBuyUndoAllButtons,
+    // renderDeleteButton
+} from '../render-utils.js';
+
+const form = document.getElementById('item-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const item = {
+        quantity: data.get('quantity'),
+        item: data.get('item'),
+        bought: false
+    };
+    await createItem(item);
+    form.reset();
+});
 
 checkAuth();
 
